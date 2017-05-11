5696e5dd1bf16eafd63f1ac6d31c4ba936ddb436
MD <mdmoctezuma@gmail.com>
2017-05-11 18:08:38 -0500

    Borro todo otra vez

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-#Cambié lo de arriba por
-#class ApplicationController < ActionController
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
-
-  include SessionsHelper
-end
--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-#class User < ApplicationRecord
-  validates :name, presence: true
-end
- 
-User.create(name: "John Doe").valid? # => true
-User.create(name: nil).valid? # => false
-#end
